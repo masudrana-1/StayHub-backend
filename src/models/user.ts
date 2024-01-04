a924f080e5ca2700ee58cb93cfdbd84db2929ad2
masudrana-1 <masudrana58855@gmail.com>
2024-01-04 18:35:25 +0600

    create user type and schema

---
+++ src/models/user.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export type UserType = {
+    _id: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+};
+
+const userSchema = new mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+});
