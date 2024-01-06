bdd0c7c1c457e969187b69ea292a3f49452eb0f7
masudrana-1 <masudrana58855@gmail.com>
2024-01-06 10:28:54 +0600

    user routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -11,9 +11,7 @@ router.post("/register", [
     check("firstName", "First Name is required").isString(),
     check("lastName", "Last Name is required").isString(),
     check("email", "Email is required").isEmail(),
-    check("password", "Password with 6 or more characters required").isLength({
-      min: 6,
-    }),
+    check("password", "Password with 6 or more characters required").isLength({ min: 6, }),
 ],async (req: Request, res: Response) => {
     // get error from validation 
     const errors = validationResult(req);
