d4582eb29f1d18756c027bd2b6a73ea49db3ff81
masudrana-1 <masudrana58855@gmail.com>
2024-01-09 19:51:48 +0600

    fixed import problem

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,4 @@
-import { NextFunction } from "express-serve-static-core";
+import { Request, Response, NextFunction } from "express";
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies["auth_token"]
