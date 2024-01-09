822d08ae352804b53a90ec5c369b87b0905bb533
masudrana-1 <masudrana58855@gmail.com>
2024-01-09 20:00:55 +0600

    check token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,16 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken';
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies["auth_token"]
+    const token = req.cookies["auth_token"];
+    // check token 
+    if (!token) {
+        return res.status(401).json({ message: "unauthorized" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+    } catch (error) {
+        return res.status(401).json({ message: "unauthorized" });
+    }
 }
