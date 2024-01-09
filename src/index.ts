7bfabde8705abdc47e645233ae957a39082bb5e1
masudrana-1 <masudrana58855@gmail.com>
2024-01-09 19:34:01 +0600

    add cookie in cors and env

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,10 @@ mongoose.connect(process.env.MONGODB_CONNECTION as string)
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}));
 
 
 // user login route 
